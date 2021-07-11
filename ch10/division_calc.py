3fd52ea333f5ee4087f335615623a3b7e8c7cfc5
Huberdoggy <huberdoggy@gmail.com>
2021-07-11 10:24:44 -0500

    wrote a word count script/function using split() method

---
+++ ch10/division_calc.py
@@ -0,0 +1,19 @@
+import sys
+
+print("Give me two numbers, and I'll divide them."
+      " (or enter 'q' to quit)")
+
+while True:
+    first_num = input(f"\nFirst number? => ")
+    if first_num == 'q':
+        break
+    second_num = input("Second number? => ")
+    if second_num == 'q':
+        break
+    try:
+        answer = int(first_num) / int(second_num)
+    except ZeroDivisionError:
+        print(f"Cant perform zero-division on {first_num}")
+        sys.exit(1)
+    else:
+        print(answer)
---
+++ ch10/word_count.py
@@ -0,0 +1,40 @@
+from os import stat
+import sys
+
+doc_list = []
+prefix = ''
+suffix = '.txt'
+
+def count_words(filename):
+    """Count the approximate number of words in a file"""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} doesn't exist..")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"\n\tThe file {filename} has about {num_words} words.\n")
+
+while True:
+    print("Enter a filename and I will count the words"
+      " (the '.txt' has been automatically included for you.")
+    choice = input("(or 'q' to quit and stop adding files) => ")
+    if choice == 'q':
+        break
+    prefix = 'textdocs/{0}{1}'.format(choice, suffix)
+    try:
+        if stat(prefix).st_size > 0:
+            print(f"File name is not empty. Just to inform you..")
+    except FileNotFoundError:
+        print("Cant find that one..")
+        break
+    else:
+        doc_list.append(prefix)
+        print(f"Now adding {prefix} to list")
+        print(doc_list)
+        prefix = ''
+print(f"\n\t***THE WORD COUNT OF BOOKS IN THE LIST***")
+for doc in doc_list:
+    count_words(doc)
