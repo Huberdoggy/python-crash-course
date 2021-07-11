97a24d17a862a27c8fea39e3a8a1d08573c8c987
Huberdoggy <huberdoggy@gmail.com>
2021-07-11 08:15:12 -0500

    some work from last night, forgot to push

---
+++ ch10/guest_prompts.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+filename = 'textdocs/guest_book.txt'
+current_date = datetime(year=2021, month=7, day=10)
+line_num = 1
+
+while True:
+    prompt = input("Please enter your name"
+                   " (or enter 'q' to quit) => ")
+    if prompt != 'q':
+        name = prompt
+        print(f"Hello there {name.title()}!")
+        with open(filename, 'a+') as file_object:
+            file_object.write(f"\n{line_num} - {name.title()} was logged as using the program on {current_date}")
+            line_num += 1
+    else:
+        break
+
+
+
