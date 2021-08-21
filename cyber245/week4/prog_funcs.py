04b91325ec9f672c04aa10c79fec28653d005afd
Huberdoggy <huberdoggy@gmail.com>
2021-08-21 11:29:30 -0500

    added to ignore and made minor formatting change in prog_funcs

--- cyber245/week4/prog_funcs.py
+++ cyber245/week4/prog_funcs.py
@@ -2,7 +2,7 @@ from termcolor import colored
 
 def conv_to_kilos(miles):
     kilos = miles * 1.609344 # Conversion factor of 1.6xx kilos for every mile
-    print(colored(f"\n\t***CONVERSION RESULT***\n", 'green'))
+    print(colored(f"\n\t\t***CONVERSION RESULT***\n", 'green'))
     print('\t' + '-' * 40)
     print(f"\tYour distance driven in miles: {miles}\n"
           f"\tEquivalent number of kilometers: {kilos:.3f}")
