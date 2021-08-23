d53ce84104b61526e0821e0e8041e0207c6eb6f3
Huberdoggy <huberdoggy@gmail.com>
2021-08-23 18:10:12 -0500

    finished most of the program. It directs users to submenu of options after prompting for all class attribs from input, and adds them as a nested list

--- cyber245/week6/cars_and_trucks.py
+++ cyber245/week6/cars_and_trucks.py
@@ -1,3 +1,4 @@
+import re
 from os import system, name
 from termcolor import cprint, colored
 from pyfiglet import Figlet
@@ -5,6 +6,13 @@ from pyfiglet import Figlet
 print_green = lambda x: cprint(x, 'green') # Simple lambda to quickly convert
 # print call color for 'x' argument provided
 print_red = lambda x: cprint(x, 'red')
+
+pattern = "^.[a-z]{4}.[a-z]{3}.[a-z]{6}\.Car.*" # for matching whether 'Car' or 'Truck' obj is passed to get_attribs func
+
+try:
+    re.compile(pattern)
+except re.error:
+    print("Invalid regex expression")
 
 def screen_clear():
     # Function to accomplish the same thing as running term clear command to prevent clutter in prog
@@ -26,8 +34,13 @@ def format_main_menu(render_str, m_dict):
 
 
 def get_vehicle_attribs(vehicle):
-    vehicle_obj = [vehicle.getMake().title(), vehicle.getModel().title(),
-               vehicle.getColor(), vehicle.getFuelType()]
+    # print(vehicle)
+    if re.fullmatch(pattern, str(vehicle)):
+        vehicle_obj = [vehicle.make.title(), vehicle.model.title(),
+               vehicle.color, vehicle.fuelType, vehicle.engineSize, vehicle.numDoors, vehicle.options]
+    else:
+        vehicle_obj = [vehicle.make.title(), vehicle.model.title(),
+                       vehicle.color, vehicle.fuelType, vehicle.cabStyle, vehicle.bedLength, vehicle.options]
     return vehicle_obj
     # This was they only way I could get it to not print the literal class object
     # I was going to use __str__ overrides in constructor but wasn't quite sure howto work it in
@@ -38,26 +51,133 @@ def get_vehicle_attribs(vehicle):
 # Parent constructor. Options are common to all vehicles and the user must choose at least 1 regardless of car/truck
 class Vehicle:
 
-    def __init__(self, fuelType='premium octane gas'):
-        self.make = input("Enter the make => ")
-        self.model = input("Enter the model => ")
-        self.color = input("Enter the color => ")
-        self.fuelType = fuelType
-        self.options = ['Power Mirrors', 'Power Steering', 'Cruise Control', 'Remote Start'
+    def __init__(self, engineSize=None, numDoors=None, cabStyle=None, bedLength=None):
+        # These will be overridden by child classes
+        self.engineSize = engineSize
+        self.numDoors = numDoors
+        self.cabStyle = cabStyle
+        self.bedLength = bedLength
+        self.options = ['Power Mirrors', 'Power Steering', 'Cruise Control', 'Remote Start',
                         'Backup Camera', 'Bluetooth', 'Anti-Lock Brakes', 'Heated Mirrors']
 
 
+    def build_opts_menu(self): # First, I'm going to build a dict of opts, similar to main-menu
+        my_dict = {}
+        count = 0
+        for option in self.options:
+            my_dict[count] = option
+            count += 1
+        self.options = my_dict
+        return my_dict
+
+
     def getMake(self):
+        self.make = input("Enter the make => ")
         return self.make
 
     def getModel(self):
+        self.model = input("Enter the model => ")
         return self.model
 
     def getColor(self):
+        self.color = input("Enter the color => ")
         return self.color
 
     def getFuelType(self):
+        f_type = input("E-85 or gas (e/g)? => ")
+        if f_type == 'g':
+            self.fuelType = 'premium octane gas'
+        else:
+            self.fuelType = 'E-85'
         return self.fuelType
 
-    def getOptions(self):
-        pass
+    def getOptions(self, render_str, m_dict): # Now I'm passing the dictionary to my 'cool formatting' func
+        screen_clear()
+        f = Figlet(font='standard', justify='center')
+        print(f"{colored(f.renderText(render_str), 'blue')}\n")
+        print_green(f"\tPlease enter numbers corresponding to the following options\n"
+                    f"\tto add to the vehicle. You must choose 1 at minimum:\n")
+        print("\t" * 3 + ("*" * 30))
+        for key in m_dict:
+            print_green("\t" * 3 + str(key) + ' - ' + m_dict[key])
+        print("\t" * 3 + ("*" * 30))
+        chosen_opt_lst = []
+        try:
+            show_menu = True
+            while show_menu:
+                num_choice = int(input("=> "))
+                if num_choice in range(0, 8):
+                    num_choice = m_dict[num_choice]
+                    chosen_opt_lst.append(num_choice)
+                    if len(chosen_opt_lst) >= 1:
+                        add_more = input("Add more? (y/n)? => ")
+                        if add_more == 'n':
+                            return chosen_opt_lst
+                else:
+                    print_red(f"{num_choice} is an invalid choice.")
+        except ValueError:
+            print("Failed to register input")
+
+
+class Car(Vehicle):
+    """ The assignment is worded sort of confusing but essentially, I'm not going to use the parent class
+    for anything other than options and functions for this 'input' to inherit correctly"""
+    def __init__(self):
+        super().__init__()
+        self.make = Car.getMake(self)
+        self.model = Car.getModel(self)
+        self.color = Car.getColor(self)
+        self.fuelType = Car.getFuelType(self)
+        self.engineSize = Car.getEngineSize(self)
+        self.numDoors = Car.getNumDoors(self)
+        car_opts_dict = Car.build_opts_menu(self)
+        self.options = Car.getOptions(self, 'Vehicle Options', car_opts_dict)
+
+    def getEngineSize(self):
+        e_size = int(input("Drive a V6 or V8 (6/8)? => "))
+        if e_size == 6:
+            self.engineSize = 'V6'
+        else:
+            self.engineSize = 'V8'
+        return self.engineSize
+
+    def getNumDoors(self):
+        num_doors = int(input("2 or 4 doors (2/4)? => "))
+        if num_doors == 4:
+            self.numDoors = "four"
+        else:
+            self.numDoors = "two"
+        return self.numDoors
+
+
+
+class Truck(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self.make = Truck.getMake(self)
+        self.model = Truck.getModel(self)
+        self.color = Truck.getColor(self)
+        self.fuelType = Truck.getFuelType(self)
+        self.cabStyle = Truck.getCabStyle(self)
+        self.bedLength = Truck.getBedLength(self)
+        truck_opts_dict = Truck.build_opts_menu(self)
+        self.options = Truck.getOptions(self, 'Vehicle Options', truck_opts_dict)
+
+    def getCabStyle(self):
+        c_style = input("Crew cab or extended (c/e)? => ")
+        if c_style == 'c':
+            self.cabStyle = 'crew cab'
+        else:
+            self.cabStyle = 'extended cab'
+        return self.cabStyle
+
+
+    def getBedLength(self):
+        b_length = input("Standard bed or long bed (s/l)? => ")
+        if b_length == 's':
+            self.bedLength = 'standard bed'
+        else:
+            self.bedLength = 'long bed'
+        return self.bedLength
+
+
--- cyber245/week6/vehicle_garage.py
+++ cyber245/week6/vehicle_garage.py
@@ -1,7 +1,8 @@
 import sys
 from termcolor import cprint, colored
 from time import sleep
-from cars_and_trucks import screen_clear as s_c, format_main_menu as fmm, get_vehicle_attribs as get_attribs, Vehicle
+from cars_and_trucks import screen_clear as s_c, format_main_menu as fmm,\
+get_vehicle_attribs as get_attribs, Car, Truck
 sys.path.insert(1, '/home/huberdoggy/python-projects/python-crash-course/cyber245/week9') # for my previous functions
 from cli_functions import make_menu as mm
 
@@ -12,6 +13,8 @@ opts_lst = ['Build a Car', 'Build a Pickup Truck', 'Quit'] # To pass to 'mm' as 
 welcome_str = "Kyle\'s Virtual Garage"
 farewell = "Thank you for using the app. Good-bye!"
 vehicle_garage = {} # Will hold completed vehicles as key/vals
+car_count = 0 # global so that if user re-enters after backing out to main menu, the count still holds
+truck_count = 0
 
 #Build main menu
 main_menu_dict = mm(opts_lst[0], opts_lst[1], opts_lst[2])
@@ -28,33 +31,60 @@ try:
                 sleep(3)
                 s_c()
                 check_done = False
-                car_count = 1
                 while not check_done:
-                    new_car = Vehicle()
-                    vroom = input(f"Your new {new_car.getMake().title()}, {new_car.getModel().title()} "
-                      f"is {new_car.getColor()} and runs on {new_car.getFuelType()}."
-                      f" Add this this car to the garage (y/n)? => ")
+                    new_car = Car()
+                    vroom = input(f"Your new {new_car.make.title()} {new_car.model.title()} "
+                      f"is {new_car.color} and runs on {new_car.fuelType}.\nIt has a {new_car.engineSize} engine"
+                                  f" and {new_car.numDoors} doors.\n"
+                                  f"Your options: {new_car.options}\nAdd this car to the garage (y/n)? => ")
                     if vroom == 'y':
+                        car_count += 1
                         add_car = get_attribs(new_car)
-                        vehicle_garage[f'Vehicle {str(car_count)}'] = add_car
+                        vehicle_garage[f'Car {str(car_count)}'] = add_car
                         print_green(f"\n\t*** CURRENT GARAGE ***\n")
                         for k,v in vehicle_garage.items():
                             print(f"{k} -> {str(v)}\n")
                         add_another = input("Add more cars? (y/n) => ")
-                        if add_another == 'y':
-                            car_count += 1
-                        else:
+                        if add_another == 'n':
                             check_done = True
                             s_c()
                             fmm(welcome_str, main_menu_dict)
                             show_menu = True # Run from the top
+                    elif vroom == 'n' and car_count < 1:
+                        print_red(f"Discarded the {new_car.model.title()}. But, you must add at least 1"
+                              f" car to the garage.")
                     else:
-                        print(f"Discarded the {new_car.getModel().title()}.")
+                        print_red(f"Discarded the {new_car.model.title()}.")
             else: # For truck creation
                 color_choice = colored('Truck', 'green')
                 print(f"You will now be directed to {color_choice} specific questions...")
                 sleep(3)
                 s_c()
+                check_done = False
+                while not check_done:
+                    new_truck = Truck()
+                    vroom = input(f"\nYour new {new_truck.make.title()} {new_truck.model.title()} "
+                                  f"is {new_truck.color} and runs on {new_truck.fuelType}.\n"
+                                  f"It has a {new_truck.cabStyle} and a {new_truck.bedLength}.\n"
+                                  f"Your options: {new_truck.options}\nAdd this truck to the garage (y/n)? => ")
+                    if vroom == 'y':
+                        truck_count += 1
+                        add_truck = get_attribs(new_truck)
+                        vehicle_garage[f'Truck {str(truck_count)}'] = add_truck
+                        print_green(f"\n\t*** CURRENT GARAGE ***\n")
+                        for k,v in vehicle_garage.items():
+                            print(f"{k} -> {str(v)}\n")
+                        add_another = input("Add more trucks? (y/n) => ")
+                        if add_another == 'n':
+                            check_done = True
+                            s_c()
+                            fmm(welcome_str, main_menu_dict)
+                            show_menu = True # Run from the top
+                    elif vroom == 'n' and truck_count < 1:
+                        print_red(f"Discarded the {new_truck.model.title()}. But, you must add at least 1"
+                              f" truck to the garage.")
+                    else:
+                        print_red(f"Discarded the {new_truck.model.title()}.")
         elif num_choice == 3:
             sys.exit(0)
         else:
