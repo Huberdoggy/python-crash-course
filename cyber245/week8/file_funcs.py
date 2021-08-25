e9df71aa5517d23c710ae4c0d94ed837500049a4
Huberdoggy <huberdoggy@gmail.com>
2021-08-25 08:51:26 -0500

    wrote a simple file reader/writer prog which utilizes OS to expand env vars for 'Documents' path

---
+++ cyber245/week8/file_funcs.py
@@ -0,0 +1,57 @@
+import re, json, sys, os
+from os import name
+from termcolor import colored
+suffix = '.txt'
+
+def add_to_path(path_str):
+    count = 1
+    for i in range(0, 2):
+        sys.path.insert(count, path_str[i])
+        count += 1
+
+
+def expand_env_vars(path_str): # To keep it open, I wanted to expand the env var for 'Documents' depending on the
+    # context where the user's running the program. Instead of blindly typing base directories and preventing typos,
+    # this way, we can start in /home/'username'/Documents and customize a file name from there
+    if name == 'nt':
+        windows = 'C:\\Users\\$USERNAME\\Documents'
+        exp_file_path = os.path.expandvars(windows)
+    else:
+        exp_file_path = os.path.expandvars(path_str)
+    return exp_file_path
+
+def does_fpath_exist(path_str, pattern_dict):
+    if os.path.isdir(path_str):
+        while True: # Technically, this will only go until a file-name matches my naming convention, since it will
+            # return 'filename' at that point
+            filename = input(f"Will store your file under {colored(path_str, 'green')}.\n"
+                         f"Please enter a file name (or 'q' to quit) => ")
+            if filename == 'q':
+                print("Come back soon. Good-bye!")
+                sys.exit(0)
+            elif re.fullmatch(pattern_dict.get('filename_input', 'empty'), filename.strip()):
+                full_f_path = path_str + '/' + filename.strip() + suffix
+                print(f"Full file path will be: {colored(full_f_path, 'green')}")
+                return full_f_path
+            else:
+                print(colored(f"File naming convention for {filename.strip()} is invalid.", 'red'))
+    else:
+        print(colored(f"Input directory {path_str} does NOT exist.", 'red'))
+        return None
+
+def store_data(file_name, user_info): # take input vars and the full file path from above function.
+    with open(file_name, 'w') as f:
+        json.dump(user_info, f)
+    print(colored(f"Successfully dumped to {file_name}", 'green'))
+    with open(file_name) as f:
+        info_dict = json.load(f)
+        line = '' # I'm doing this so that I can construct a 'return' with the full formatted info sep by \n
+        for key in info_dict:
+            line += f"{key}: {info_dict[key]}\n"
+        return f"\n*** CURRENT CONTENTS OF: {colored(file_name, 'green')} ***\n\n{line}"
+
+
+
+
+
+
---
+++ cyber245/week8/file_write_read.py
@@ -0,0 +1,58 @@
+import re, sys
+from termcolor import colored
+from file_funcs import add_to_path as atp, expand_env_vars as exp_vars, \
+    does_fpath_exist as does_exist, store_data
+mod_paths = ['/home/huberdoggy/python-projects/python-crash-course/cyber245/week9',
+                             '/home/huberdoggy/python-projects/python-crash-course/cyber245/week6']
+atp(mod_paths) # Quickly insert addl paths into $PYTHONPATH for my other module dirs
+from cli_functions import compile_patterns as c_p
+from cars_and_trucks import screen_clear as s_c
+reg_patterns = {
+    'filename_input': '^[a-zA-Z]{1,}(_|\-)?(\d{1,})?$',
+    'name': '^[a-zA-Z]{2,}$',
+    'address': '^[0-9]{1,}\s[a-zA-Z0-9]{1,15}\s[a-zA-Z]{1,15}\s?(\w{1,5})?$', # this should cover enough
+    # --> house number, space, street num with suffix, space, street name,
+    # optional - space and cardinal direction
+    'phone_num': '^\d{3}\d{3}\d{4}$', # for user convenience, I wont require hyphens or literal parenthesees
+}
+
+user_info = {}
+
+c_p(reg_patterns)
+prog_file_path = exp_vars('$HOME/Documents') # Pass string to func to expand $HOME on Linux
+s_c()
+full_file_path = does_exist(prog_file_path, reg_patterns) # will return True if my base-path variable exists and
+# build filename + suffix from user input
+name_valid, address_valid, phone_valid = False, False, False
+while not name_valid:
+    get_name = input("Now, what's your name? => ")
+    if re.fullmatch(reg_patterns.get('name', 'empty'), get_name):
+        user_info['Name'] = get_name
+        name_valid = True
+    else:
+        print(colored(f"{get_name.strip()} is not the correct format for 'name'.", 'red'))
+        continue
+    while not address_valid:
+        get_addy = input("Now, whats your address? (space separated)? => ")
+        if re.fullmatch(reg_patterns.get('address', 'empty'), get_addy):
+            user_info['Address'] = get_addy
+            address_valid = True
+        else:
+            print(colored(f"{get_addy.strip()} is not the correct format for 'address'.", 'red'))
+            continue
+        while not phone_valid:
+            get_phone = input("Please enter your phone number.\nFor your convenience, I require digits only => ")
+            if re.fullmatch(reg_patterns.get('phone_num', 'empty'), get_phone):
+                user_info['Phone'] = get_phone
+                phone_valid = True
+            else:
+                print(colored(f"{get_phone.strip()} is not the correct format for 'phone'.", 'red'))
+
+dump_display_data = store_data(full_file_path, user_info)
+print(dump_display_data)
+
+
+
+
+
+
